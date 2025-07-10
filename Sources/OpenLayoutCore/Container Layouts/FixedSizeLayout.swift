ada5491bff387e02032fe9589ecf6ae70064f64c
Andrii Zinoviev <azinoviev@readdle.com>
2025-07-10 18:06:08 +0200

    Fixed Size

---
+++ Sources/OpenLayoutCore/Container Layouts/FixedSizeLayout.swift
@@ -0,0 +1,37 @@
+import CoreGraphics
+
+public struct FixedSizeLayout: UnaryLayout {
+    public let horizontal: Bool
+    public let vertical: Bool
+    
+    public init(horizontal: Bool, vertical: Bool) {
+        self.horizontal = horizontal
+        self.vertical = vertical
+    }
+    
+    public func sizeThatFits(
+        _ proposal: ProposedSize,
+        child: some LayoutSizeProvider
+    ) -> CGSize {
+        child.sizeThatFits(self.childProposal(for: proposal))
+    }
+    
+    public func placeChild(in rect: CGRect, child: inout some LayoutElement) {
+        child.place(
+            at: rect.center,
+            anchor: .center,
+            proposal: self.childProposal(for: ProposedSize(rect.size))
+        )
+    }
+    
+    private func childProposal(for proposal: ProposedSize) -> ProposedSize {
+        var childProposal = proposal
+        if self.horizontal {
+            childProposal.width = nil
+        }
+        if self.vertical {
+            childProposal.height = nil
+        }
+        return childProposal
+    }
+} 
---
+++ Sources/OpenLayoutDSL/Containers/FixedSize.swift
@@ -0,0 +1,15 @@
+import CoreGraphics
+import OpenLayoutCore
+
+extension LayoutItem {
+    public func fixedSize() -> some LayoutItem {
+        self.fixedSize(horizontal: true, vertical: true)
+    }
+    
+    public func fixedSize(horizontal: Bool, vertical: Bool) -> some LayoutItem {
+        UnaryContainerItem(
+            layout: FixedSizeLayout(horizontal: horizontal, vertical: vertical),
+            child: self
+        )
+    }
+} 
